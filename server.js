b8e9b0aba7096220a269838e7282cb86208f272a
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2021-12-04 13:55:56 +0200

    Add files via upload
    Ryan has some custom functions like to type a world you just have to type q("") and my functions name's are kinda funny

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-console.log("poatao");
-let Name = "Ryan";
-let Age = "14";
-let Emoji = "👍";
-console.log(Name);
-console.log(Age);
-console.log(Emoji);
+function q(word) {
+  console.log(word);
+}
+function le(mutaghayer) {
+  let(mutaghayer);
+}
+
 const fs = require("fs");
-let data = fs.readFilesSync("db.json");
+let data = fs.readFileSync("db.json");
 let users = JSON.parse(data);
 
-console.log(users);
+q(users);
 
 function MakePerson(id, name, city) {
   users[id] = {
@@ -17,7 +17,34 @@ function MakePerson(id, name, city) {
     city,
   };
 }
-
+function readUser(id) {
+  return users[id];
+}
 MakePerson(1, "Person1", "NOT AMERICA  ");
 MakePerson(2, " Person2", "AGAIN NOT AMERICA");
-console.log(users);
+MakePerson(3, "Person3", "NOT AMERICA  ");
+MakePerson(4, " Person4", "AGAIN NOT AMERICA");
+MakePerson(5, "Person5", "NOT AMERICA  ");
+
+q(users);
+
+function BoomHeadshot(id) {
+  delete users[id];
+}
+
+function RemakePerson(id, name, city) {
+  if (users[id] == undefined) {
+    return "Buddy, That person doesn't exist";
+  } else {
+    users[id] = {
+      name: name,
+      city: city,
+    };
+  }
+}
+
+RemakePerson(1, "RyanM", "PotatoLands");
+q(users);
+
+let dataToString = JSON.stringify(users);
+fs.writeFileSync("db.json", dataToString);
